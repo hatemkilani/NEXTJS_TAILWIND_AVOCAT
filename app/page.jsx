8f7a58db84e25bef6f4459e1eb5b80c085d21928
hatemkilani <hatem.kilani1999l@gmail.com>
2024-08-12 11:52:07 +0200

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Hatem Kilani is Zabour
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
